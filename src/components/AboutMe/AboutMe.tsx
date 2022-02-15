99f9773f9aff8ab1663cfaee66ac81a5faef3aee
kamilsulgut <kamilsulgut@gmail.com>
2022-02-15 13:48:01 +0100

    Add about me identify and position

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
 import AboutImg from "./AboutImg";
 import AboutText from "./AboutText";
@@ -6,6 +6,17 @@ import AboutText from "./AboutText";
 const AboutMe = () => {
   return (
     <>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 3,
+          color: "white",
+        }}
+      >
+        <Typography variant="h3">ABOUT ME</Typography>
+      </Box>
       <Box
         sx={{
           display: "flex",

7a4d5039fba5f3bedd7585cc61117fa4b36dbf16
kamilsulgut <kamilsulgut@gmail.com>
2022-02-16 10:59:07 +0100

    Add Portfolio component

---
+++ src/components/Portfolio/Portfolio.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import React, { FC } from "react";
+import { useStyles } from "../../constants/styles";
+
+const Portfolio: FC = () => {
+  const styles = useStyles();
+  return <Box></Box>;
+};
+
+export default Portfolio;
